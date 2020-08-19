5c5e9de6749ab77c566794ef6827d9e8960816a3
Saurabh-think-different <thefunguywholovesmustang@gmail.com>
2020-08-19 13:17:01 +0530

    filter option added

--- index.js
+++ index.js
@@ -3,10 +3,13 @@
 const todoInput  = document.querySelector(".todo-input");
 const todoButton  = document.querySelector(".todo-button");
 const todoList  = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
 
 //Event Listeners
-todoButton.addEventListener("click", addTodo);
+todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteAndCheck);
+filterOption.addEventListener('click', filterTodo);
 
 //Functions
 function addTodo(event){
@@ -59,4 +62,31 @@ function deleteAndCheck(e){
         const itemParent = item.parentNode;
         itemParent.classList.toggle('completed');
     }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')){
+                    todo.style.display = "flex";
+                }
+                else{
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('completed')){
+                    todo.style.display = "flex";
+                }
+                else{
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
 }
